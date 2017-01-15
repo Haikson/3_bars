a5aaddb1027c9a520dea2596c4aa34f48448ff30
Kamo Petrosyan <kamo@haikson.com>
2017-01-16 00:28:20 +0600

    modified:   bars.py 	new file:   data-2897-2016-11-23.json 	new file:   data-2909-2016-11-23.zip

--- bars.py
+++ bars.py
@@ -1,21 +1,44 @@
+#-*- coding: utf-8 -*-
+import sys
 import json
+import math
 
 
 def load_data(filepath):
-    pass
+    fd = open(filepath)
+    data = json.load(fd, encoding="utf-8")
+    fd.close()
+    return data
 
 
 def get_biggest_bar(data):
-    pass
+    print(max((bar['SeatsCount'] for bar in data)))
 
 
 def get_smallest_bar(data):
-    pass
+    print(min((bar['SeatsCount'] for bar in data)))
+
+
+def distance(longitude, latitude, bar):
+    coordinates = bar["geoData"]["coordinates"]
+    return math.sqrt(pow((coordinates[0]-longitude), 2)+pow((coordinates[1]-latitude), 2))
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
-
+    min_distance = distance(longitude, latitude, data[0])
+    closest_bar = data[0]
+    for bar in data:
+        dist = distance(longitude, latitude, bar)
+        if dist < min_distance:
+            min_distance = dist
+            closest_bar = bar
+    print closest_bar["Name"]
+            
 if __name__ == '__main__':
-    pass
+    data = load_data(filepath="data-2897-2016-11-23.json")
+    get_biggest_bar(data)
+    get_smallest_bar(data)
+
+    longitude = input()
+    latitude = input()
+    get_closest_bar(data, longitude, latitude)
